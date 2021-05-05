ff4a197224998326ceac8a089d913c2a31c9ba64
krishnadavuluri <kitts.davuluri@gmail.com>
2021-05-05 10:34:18 +0530

    pagination changed to scrool bar

--- src/components/MasterCount.jsx
+++ src/components/MasterCount.jsx
@@ -2,48 +2,86 @@ import React from 'react';
 import MasterCountChart from './charts/MasterCountChart';
 import '../styles/styling.css';
 import Service from '../utils/service';
+import { Grid } from '@material-ui/core';
+import { MDBContainer }from "mdbreact";
 export default class MasterCount  extends React.Component{
     constructor(props)
     {
        super(props);
        this.state={
-           countData:{},
+           itemsProgress:[],
+           itemsLabel:[],
+           end:0,
+           current:0,
            flag:false,
        }
+       this.handlePreviousButton=this.handlePreviousButton.bind(this);
+       this.handleBackButton=this.handleBackButton.bind(this);
+    }
+    handlePreviousButton()
+    {
+
+         this.setState({current:this.state.current-1})
+        
+    }
+    handleBackButton()
+    {
+            this.setState({current:this.state.current+1})   
     }
     componentDidMount()
     {
+       // console.log(this.props.language)
         const itemsData=this.props.data;
-        const colors=Service.colourGenerator(itemsData.length);
         var itemsCount=[];
         var itemsName=[];
         itemsData.map((eachItem)=>{                                                            //destructuring data
             itemsCount=[...itemsCount,Math.ceil((eachItem.completedCount/eachItem.count)*100)]
             itemsName=[...itemsName,'#'+`${eachItem.id} `]
         })
-        this.setState({countData:
+        const divide=Math.ceil((itemsCount.length)/8)
+        const partition=Math.floor((itemsCount.length)/divide)
+        console.log(partition)
+        var i=0,start=0,end=partition;
+        var count=[];
+        var labels=[];
+        while(i<divide)
         {
-           datasets:[
-               {                                  //setting state
-                   data:itemsCount,
-                   backgroundColor:colors,
-                   label:'My data'
-               }],
-               labels:itemsName  
-        },flag:true})
+            if(i===divide-1)
+            {
+               count=[...count,itemsCount.slice(start,)]
+               labels=[...labels,itemsName.slice(start,)]
+               break;
+            }
+            count=[...count,itemsCount.slice(start,end)]
+            labels=[...labels,itemsName.slice(start,end)]
+            start=end;
+            end+=partition;
+            i+=1;
+        }
+        console.log('count and labels',count,labels);
+        this.setState({flag:true,itemsProgress:count,itemsLabel:labels,end:count.length})
     }
     render()
     {
         return(
             <div className='defaultMargin'>
-                {this.state.flag ? 
-                 <>
-                 <MasterCountChart language={this.props.language} 
-                                   noOfItems={this.props.data.length}  
-                                   data={this.state.countData} /> 
-                 <button className='Button'>{'<'}</button>
-                 <button className='Button'>{'>'}</button>   
-                 </>: ''}
+                <h3 style={{marginTop:'60px'}}>Completion Percentage</h3>
+                {
+                    this.state.flag? 
+                    <Grid container justify='center' >
+                        <Grid item justify='center' xs={12} sm={12}  className='CountChartBox' md={6} >
+                           <Grid container justify='center'>
+                                {
+                                  this.state.itemsProgress.map((value,index)=>(
+                                  
+                                   <MasterCountChart language={this.props.language} number={index+1}
+                                   data={this.state.itemsProgress[index]} labels={this.state.itemsLabel[index]} />
+                                  ))
+                               }   
+                           </Grid>
+                        </Grid>
+                    </Grid> :''   
+               }
             </div>
         );
     }
--- src/components/charts/MasterCountChart.jsx
+++ src/components/charts/MasterCountChart.jsx
@@ -5,40 +5,48 @@ import Grid from '@material-ui/core/Grid';
 import '../../styles/styling.css';
 import { IntlProvider, FormattedMessage } from 'react-intl';
 import { LangMessage } from '../../locale/locale';
-import { TestChart } from '../TestChart';
-function MasterCountChart({data,noOfItems,language})
+import Service from '../../utils/service';
+function MasterCountChart({data,labels,number})
 {
     var height=150;                 //default height
     const width=window.innerWidth;        //getting width of screen
+    const colors=Service.colourGenerator(data.length);
     console.log('Data:',data);
-    console.log(language);
     if(width<=640)
     {
-        height=35*noOfItems;              //setting height of chart with respect width of screen
+        height=40*10;              //setting height of chart with respect width of screen
     }
     else if(width>640){
-       height=20*noOfItems;
+       height=35*10;
     }
+    const Data={
+        datasets:[
+            {                                  //setting state
+                data:data,
+                backgroundColor:colors,
+                label:'Hello'
+            }],
+            labels:labels
+     }
     const options={
          responsive: true,
-        //  onClick:(evt)=>{
-        //     console.log(evt)
-        // }
+         title: {
+            display: true,
+            text: `Showing ${number}st part`
+        }
     }
     return (
     <Grid container justify='center'>
-        <Grid item xs={12} sm={12} md={5} style={{border:'none'}} >
-           <MDBContainer >
-              <h3 className="mt-5 font">
-                  <IntlProvider locale={language} messages={LangMessage[language]}>
-                  <FormattedMessage id='completionPercentage' value={language}/>
-                  </IntlProvider>
-              </h3>
-              <Polar id="Polar" data={data} height={height}  options={ options }/>
-              {/* <TestChart/> */}
-           </MDBContainer>
+        <Grid item xs={12} md={7}>
+           <MDBContainer>
+               <Polar id="Polar" data={Data} height={height}  options={ options }/>
+            </MDBContainer>  
         </Grid>
-    </Grid>  
+        
+    </Grid>
+             
+         
+      
     );
 }
 export default MasterCountChart;
