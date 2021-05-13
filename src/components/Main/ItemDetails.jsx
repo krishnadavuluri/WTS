8b450d78990c180afa7ac07b71e861f182b1293a
krishna kumar <kitts.davuluri@gmail.com>
2021-05-13 21:22:47 +0530

    Code pushed

--- src/components/Main/ItemDetails.jsx
+++ src/components/Main/ItemDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table } from '../Table/Table';
+import { Table } from '../Table/Table'
 import '../../styles/table.css'
 const ItemDetails = ({data,language}) => {
     return (
